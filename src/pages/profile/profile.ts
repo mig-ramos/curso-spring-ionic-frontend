4a53deb039c2709335be318abc1eac791f4026d8
Miguel Arcanjo <mig.ramos10@gmail.com>
2021-02-01 16:00:29 -0300

    Tratando erros 403

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -29,7 +29,15 @@ export class ProfilePage {
         this.cliente = Response;
         this.getImageIfExists();
       },
-      error => {});
+      error => {
+        if (error.status == 403){
+          this.navCtrl.setRoot('HomePage');
+        }
+      });
+      
+    }
+    else {
+      this.navCtrl.setRoot('HomePage');
     }
   }
 
