603ab942c14cbd10e8a62585debda93a9816f563
mig-ramos <mig.ramos10@gmail.com>
2021-02-07 21:29:24 -0300

    Fazendo upload da foto

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -26,6 +26,10 @@ export class ProfilePage {
   }
 
   ionViewDidLoad() {
+    this.loadData();
+  }
+
+  loadData(){
     let localUser = this.storage.getLocalUser();
     if (localUser && localUser.email){
       this.clienteService.findByEmail(localUser.email)
@@ -69,5 +73,19 @@ export class ProfilePage {
      this.cameraOn = false;
     }, (err) => {
     });
+  }
+
+  sendPicture(){
+    this.clienteService.uploadPicture(this.picture)
+    .subscribe(response => {
+      this.picture = null;
+      this.loadData();
+    },
+    error => {      
+    })
+  }
+
+  cancel(){
+    this.picture = null;
   }
 }
